6d43ba8adf393ce46034359af9a21bea205450b1
Mira <a.miroslawaaa@gmail.com>
2022-10-12 19:49:21 +0300

    Added Footer component

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <p class="footer">
+      <a href="https://github.com/misxleading/weather-app-project">
+        Open-source code
+      </a>
+      by Myroslava Antoniva
+    </p>
+  );
+}
+
+export default Footer;
